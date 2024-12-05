c96ed7b16014a9c9c4f31b2de3ad5133bc01c65a
FreeNFF <nauisfricsons30@gmail.com>
2024-12-05 11:20:44 +0200

    s

--- 4csvi.py
+++ 4csvi.py
@@ -13,3 +13,19 @@ print()
 print("---------------------------------------------------------------")
 print()
 print("Masīva garums: ",len(saraksts))
+
+print()
+print("---------------------------------------------------------------")
+print()
+pedejie3 = saraksts[-4:-1]
+print("Pēdējās 3 rindiņas:\n",pedejie3)
+
+def Nosaukums():
+    print('Faila nosaukums: ',file.name)
+
+Nosaukums()
+
+def Pirmais():
+    pirmais = saraksts[1]
+    print("Pirmā rindiņa:\n",pirmais)
+Pirmais()
